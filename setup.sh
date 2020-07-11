07d316fa9058329d77cf6485f5c8637ea33c710c
tfranssen <54217714+tfranssen@users.noreply.github.com>
2020-07-11 09:48:35 +0200

    Added NAT support so internet is available

--- setup.sh
+++ setup.sh
@@ -93,9 +93,19 @@ allow-hotplug wlxaca2136650d6
         wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
 EOF
 
+#####NAT en Forwarding#####
+# wlan0 is LAN
+# wlxaca2136650d6 is WAN
+echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -i wlan0 -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -A POSTROUTING -o wlxaca2136650d6 -j MASQUERADE
+iptables -A FORWARD -i wlxaca2136650d6 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wlan0 -o wlxaca2136650d6 -j ACCEPT
+
+
 #####Chirpstack installeren#####
-
-
 apt install mosquitto mosquitto-clients redis-server redis-tools postgresql -y
 
 echo "create role chirpstack_as with login password 'dbpassword';
